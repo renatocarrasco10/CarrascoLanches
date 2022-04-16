28cc7326b210b073c8a221c3a17386a1557b99a0
Renato Carrasco <renatocarrasco10@gmail.com>
2022-04-16 20:56:03 -0300

    Projeto criado

---
+++ CarrascoLanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarrascoLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
