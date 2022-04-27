6ba1775b598cd4dbb727f41abb96dc507b47b123
Renato Carrasco <renatocarrasco10@gmail.com>
2022-04-27 07:48:51 -0300

    Criando View da Login

--- CarrascoLanches/ViewModels/LoginViewModel.cs
+++ CarrascoLanches/ViewModels/LoginViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace CarrascoLanches.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarrascoLanches.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Informe o Nome")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe a Senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
